584b76179515bc86abd9aec6bae610e605f6c5c8
mckirk <mckirk@posteo.de>
2024-12-15 23:28:06 +0100

    Update lib

--- bootstrap/templates/py/lib.py
+++ bootstrap/templates/py/lib.py
@@ -5,33 +5,34 @@ from collections import defaultdict, Counter
 import aocparser
 
 script_dir = Path(__file__).parent
-spec_file = script_dir / "spec.aocp"
+default_spec_name = "spec.aocp"
 
 
 class Input:
-    def __init__(self, content: str | None):
+    def __init__(self, content: str | None, spec_name: str = None):
         self.content = content
 
         if not content:
             return
 
         self.lines = self.content.splitlines()
-        self.blocks = [Input(b) for b in self.content.split("\n\n")]
+        self.blocks = self.content.split("\n\n")
 
-        if spec_file.is_file() and (spec := spec_file.read_text()):
-            try:
-                self.parsed = aocparser.parse(spec, self.content)
-            except:
-                self.parsed = None
+        if (
+            spec_name
+            and (spec_file := script_dir / spec_name).is_file()
+            and (spec := spec_file.read_text())
+        ):
+            self.parsed = aocparser.parse(spec, self.content)
         else:
             self.parsed = None
 
     @classmethod
-    def from_file(cls, path: Path):
+    def from_file(cls, path: Path, spec_name: str = None):
         if not path.is_file():
             return cls(None)
         else:
-            return cls(path.read_text().strip())
+            return cls(path.read_text().strip(), spec_name)
 
     @property
     def exists(self):
@@ -48,11 +49,11 @@ class Input:
                 yield (x, y), c
 
 
-problem_input = Input.from_file(script_dir / "input.txt")
+problem_input = Input.from_file(script_dir / "input.txt", default_spec_name)
 
 
 def get_sample_input(idx: int):
-    return Input.from_file(script_dir / f"sample{idx}.txt")
+    return Input.from_file(script_dir / f"sample{idx}.txt", default_spec_name)
 
 
 def run_on_inputs(run, expected_sample_results: dict[int, Any] = None, **kwargs):
